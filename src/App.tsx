6edcdb423be7969eb1ca9f8035cd461714a34e51
Ehab Sommad <ehab55.sommad@gmail.com>
2023-03-01 21:14:35 +0200

    commit-hero-section-end

--- src/App.tsx
+++ src/App.tsx
@@ -114,6 +114,96 @@ function App() {
       </header>
       {/*====== END header ======*/}
 
+      {/*====== hero ======*/}
+      <section className="relative bg-white py-16 lg:pt-[100px]">
+        <div className="mx-auto max-w-7xl px-8 md:px-6">
+          <div className="flex flex-wrap">
+            <div className="w-full lg:w-5/12">
+              <h1
+                className=" text-left text-slate-800 mb-3 text-4xl font-bold leading-snug sm:text-
+            [42px] lg:text-[40px] xl:text-[42px] "
+              >
+                Everything you need to run your online{" "}
+                <span className="text-blue-600">business</span>
+              </h1>
+
+              <p className="text-left text-slate-500 mb-8 max-w-[480px] text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+                alias voluptate esse blanditiis molestiae repudiandae fugiat
+                eius sapiente expedita ut.
+              </p>
+              <div className='flow-root'>
+              <button
+                className="w-full rounded-md bg-blue-500 px-8 py-2.5 font-semibold
+            text-white shadow-md shadow-blue-500/20 hover:bg-blue-600 duration-200
+              sm:w-auto float-left"
+              >
+                Get Started
+              </button>
+              <button
+                className=" mt-4 box-border w-full rounded-md border
+              border-blue-500/20 px-8 py-2.5 font-semibold text-blue-500 shadow-md
+              shadow-blue-500/10 duration-200 sm:ml-4 sm:mt-0 sm:w-auto float-left"
+              >
+                Register Now
+              </button></div>
+              {/** brand */}
+              <div className="mt-6 flex flex-wrap gap-4 ">
+                <img
+                  src={require("./resources/images/brand/brand (1).png")}
+                  alt="brand"
+                  className="w-32
+                cursor-pointer rounded-lg border border-blue-300/20 bg-white
+                px-5 py-3 shadow-md shadow-blue-500/5 duration-200
+                hover:scale-95 sm:w-36"
+                />
+
+                <img
+                  src={require("./resources/images/brand/brand (2).png")}
+                  alt="brand"
+                  className="w-32
+                cursor-pointer rounded-lg border border-blue-300/20 bg-white
+                px-5 py-3 shadow-md shadow-blue-500/5 duration-200
+                hover:scale-95 sm:w-36"
+                />
+
+                <img
+                  src={require("./resources/images/brand/brand (3).png")}
+                  alt="brand"
+                  className="w-32
+                cursor-pointer rounded-lg border border-blue-300/20 bg-white
+                px-5 py-3 shadow-md shadow-blue-500/5 duration-200
+                hover:scale-95 sm:w-36"
+                />
+
+                <img
+                  src={require("./resources/images/brand/brand (4).png")}
+                  alt="brand"
+                  className="w-32
+                cursor-pointer rounded-lg border border-blue-300/20 bg-white
+                px-5 py-3 shadow-md shadow-blue-500/5 duration-200
+                hover:scale-95 sm:w-36"
+                />
+              </div>
+            </div>
+
+            <div className="hidden px-4 lg:block lg:w-1/12"></div>
+            <div className="w-full px-4 lg:w-6/12">
+              <div className="lg:ml-auto lg:text-right">
+                <div className="relative z-10 inline-block pt-11 lg:pt-0">
+                  <img
+                    src={require("./resources/images/hero.png")}
+                    alt="hero section image"
+                    className="max-w-full lg:ml-auto"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/*====== END hero ======*/}
+
 
     </div>
   );
